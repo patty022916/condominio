6920c3451c57de14a0544685ba6dc797ed0073b4
PATRICIA\patri <patriciaguilarojas@gmail.com>
2025-05-16 18:06:47 -0400

    cambio de hogar por edificio araguaney en menu

--- src/app/layouts/full/sidebar/sidebar-condiminio-menu.ts
+++ src/app/layouts/full/sidebar/sidebar-condiminio-menu.ts
@@ -2,7 +2,7 @@ import { NavItem } from './nav-item/nav-item';
 
 export const navItems: NavItem[] = [
     {
-        navCap: 'Hogar',
+        navCap: 'Edif. Araguaney',
     },
     {
         displayName: 'Estadísticas',
