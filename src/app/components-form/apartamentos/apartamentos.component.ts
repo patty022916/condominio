24bd2e6b43b6795e78246f9c6310c23881e9da73
Pericles <slouis482@gmail.com>
2025-06-07 15:36:32 -0400

    Formulario de apartamentos

--- src/app/components-form/apartamentos/apartamentos.component.ts
+++ src/app/components-form/apartamentos/apartamentos.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { LoadingComponent } from "../loading/loading.component";
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from 'src/app/material.module';
@@ -7,7 +7,11 @@ import { MatTableDataSource } from '@angular/material/table';
 import { Apartamentos } from 'src/app/models/Apartamentos';
 import { ApartamentosService } from 'src/app/services/apartamentos.service';
 import { ToastService } from 'src/app/services/toast.service';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonsHeaderComponent } from "../buttons-header/buttons-header.component";
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Usuario } from 'src/app/models/Usuarios';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-apartamentos',
@@ -15,16 +19,26 @@ import { FormsModule } from '@angular/forms';
     CommonModule,
     MaterialModule,
     LoadingComponent,
-    FormsModule
+    FormsModule,
+    ButtonsHeaderComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './apartamentos.component.html',
   styleUrl: './apartamentos.component.scss'
 })
 export class ApartamentosComponent {
   loading: boolean = false
+  @ViewChild('dialogTemplate') dialogTemplate!: TemplateRef<any>;
   @ViewChild(MatPaginator) paginator!: MatPaginator
-
+  dialogRef: MatDialogRef<any>;
   apartamentos: Apartamentos[] = []
+
+  apartamento: Apartamentos = new Apartamentos()
+
+  usuarios: Usuario[] = []
+  copy_usuarios: Usuario[] = []
+  search_user = new FormControl('');
+
   dataSource = new MatTableDataSource<Apartamentos>(this.apartamentos);
 
   columnasFiltro = { piso: false, letra: false, propietario: false, inquilino: false };
@@ -44,7 +58,9 @@ export class ApartamentosComponent {
 
   constructor(
     private apartamentosService: ApartamentosService,
-    private toastService: ToastService
+    private toastService: ToastService,
+    private dialog: MatDialog,
+    private usuariosService: UsuariosService
   ) { }
 
   ngAfterViewInit() {
@@ -64,6 +80,18 @@ export class ApartamentosComponent {
         this.loading = false
       },
     })
+
+    this.usuariosService.listUsers().subscribe(u => this.copy_usuarios = this.usuarios = u.filter(user => user.id_rol === 2 || user.id_rol === 3))
+
+
+    //*Filtrado del autocomplete
+    this.search_user.valueChanges.subscribe(value => {
+      const filterValue = typeof value === 'string' ? value.toLowerCase() : '';
+      this.copy_usuarios = this.usuarios.filter(usuario =>
+        usuario.nombre.toLowerCase().includes(filterValue) ||
+        usuario.piso?.toString().toLowerCase().includes(filterValue)
+      );
+    });
   }
 
   /**
@@ -85,5 +113,29 @@ export class ApartamentosComponent {
     };
 
     this.dataSource.filter = valor.trim().toLowerCase();
+  }
+
+  openModal(element?: Apartamentos) {
+    if (element) {
+      this.apartamento = { ...element }
+    } else {
+      this.apartamento = new Apartamentos()
+    }
+    this.dialogRef = this.dialog.open(this.dialogTemplate);
+  }
+
+  guardar() {
+    this.toastService.show('Apartamento guardado con éxito');
+    this.dialogRef.close();
+  }
+  displayUser(user: Usuario): string {
+    return user?.nombre || '';
+  }
+  onSelect(user: Usuario) {
+
+    if (user.id == 2) this.apartamento.inquilino_id = user.id;
+    if (user.id == 3) this.apartamento.propietario_id = user.id;
+
+
   }
 }
